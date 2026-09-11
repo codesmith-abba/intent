39929b7c1012cadf584a57174a8107d6ae25d10c
CodeSmith <114495965+codesmith-abba@users.noreply.github.com>
2026-09-11 17:53:26 +0100

    Fix initializer template to match current ITL grammar

--- itl/project/initializer.py
+++ itl/project/initializer.py
@@ -6,11 +6,13 @@ from itl.project.state import ProjectStateStore
 
 
 DEFAULT_APP_TEMPLATE = """app $__PROJECT_NAME__ {
-    page $home {
-        section $main {}
+    system {
+        frontend {
+            framework $react
+        }
     }
+
     target $web
-    framework $react
 }
 """
 
