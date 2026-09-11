509e75da7d0061f633f035d72ff727ec7a39c1d8
CodeSmith <114495965+codesmith-abba@users.noreply.github.com>
2026-09-11 15:33:57 +0100

    Add skipped build result status

--- itl/build/results.py
+++ itl/build/results.py
@@ -3,58 +3,38 @@ from enum import Enum
 
 
 class BuildResultStatus(Enum):
-
     SUCCESS = "success"
     FAILED = "failed"
+    SKIPPED = "skipped"
 
 
 @dataclass(slots=True)
 class BuildResult:
-
     source: str
-
     status: BuildResultStatus
-
     output: object | None = None
-
     error: Exception | None = None
 
 
 @dataclass(slots=True)
 class BuildResults:
+    results: list[BuildResult] = field(default_factory=list)
 
-    results: list[BuildResult] = field(
-        default_factory=list
-    )
-
-    def add(
-        self,
-        result: BuildResult,
-    ):
-
+    def add(self, result: BuildResult):
         self.results.append(result)
 
     @property
     def successful(self) -> list[BuildResult]:
-
-        return [
-            result
-            for result in self.results
-            if result.status
-            == BuildResultStatus.SUCCESS
-        ]
+        return [result for result in self.results if result.status == BuildResultStatus.SUCCESS]
 
     @property
     def failed(self) -> list[BuildResult]:
+        return [result for result in self.results if result.status == BuildResultStatus.FAILED]
 
-        return [
-            result
-            for result in self.results
-            if result.status
-            == BuildResultStatus.FAILED
-        ]
+    @property
+    def skipped(self) -> list[BuildResult]:
+        return [result for result in self.results if result.status == BuildResultStatus.SKIPPED]
 
     @property
     def succeeded(self) -> bool:
-
-        return not self.failed
+        return not self.failed and not self.skipped
