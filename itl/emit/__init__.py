ab5834bf81780de6779033872bb3b8f294e916b9
CodeSmith <114495965+codesmith-abba@users.noreply.github.com>
2026-09-11 17:24:05 +0100

    Phase 14: expose emit API

---
+++ itl/emit/__init__.py
@@ -0,0 +1,14 @@
+from itl.emit.emitter import EmitError, Emitter, UnsafeOutputPathError
+from itl.emit.mapper import OutputMapper, RelativeOutputMapper
+from itl.emit.models import EmitResult, EmitResults, EmitStatus
+
+__all__ = [
+    "EmitError",
+    "Emitter",
+    "UnsafeOutputPathError",
+    "OutputMapper",
+    "RelativeOutputMapper",
+    "EmitResult",
+    "EmitResults",
+    "EmitStatus",
+]
