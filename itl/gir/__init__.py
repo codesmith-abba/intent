b7b9abad4d863ad4a1b667356e9ffd59efea1105
CodeSmith <114495965+codesmith-abba@users.noreply.github.com>
2026-09-13 08:08:52 +0100

    feat: export authentication GIR nodes

--- itl/gir/__init__.py
+++ itl/gir/__init__.py
@@ -1,13 +1,10 @@
 from .diff import GIRDiffAnalyzer, GIRDiffCategory, GIRDiffSet, GIRFieldDiff, GIRNodeDiff
 from .fingerprint import GIRFingerprint
 from .models import (
-    GIRApplication, GIRHero, GIRNode, GIRPage, GIRSection, GIRSystem,
-    GIRField, GIRRelationship, GIRAction, GIRModel, GIRRoute, GIRPermissionRole,
+    GIRApplication, GIRHero, GIRNode, GIRPage, GIRSection, GIRSystem, GIRField, GIRRelationship, GIRAction, GIRModel, GIRRoute,
+    GIRPermission, GIRPermissionRole, GIRAuth, GIRAuthProvider, GIRRegistration, GIRLogin, GIRLogout, GIRPasswordRecovery,
+    GIRVerification, GIRSession, GIRMFA,
 )
-
 __all__ = [
-    "GIRApplication", "GIRDiffAnalyzer", "GIRDiffCategory", "GIRDiffSet",
-    "GIRFieldDiff", "GIRFingerprint", "GIRHero", "GIRNode", "GIRNodeDiff",
-    "GIRPage", "GIRSection", "GIRSystem", "GIRField", "GIRRelationship",
-    "GIRAction", "GIRModel", "GIRRoute", "GIRPermissionRole",
+    "GIRApplication", "GIRDiffAnalyzer", "GIRDiffCategory", "GIRDiffSet", "GIRFieldDiff", "GIRFingerprint", "GIRHero", "GIRNode", "GIRNodeDiff", "GIRPage", "GIRSection", "GIRSystem", "GIRField", "GIRRelationship", "GIRAction", "GIRModel", "GIRRoute", "GIRPermission", "GIRPermissionRole", "GIRAuth", "GIRAuthProvider", "GIRRegistration", "GIRLogin", "GIRLogout", "GIRPasswordRecovery", "GIRVerification", "GIRSession", "GIRMFA",
 ]
